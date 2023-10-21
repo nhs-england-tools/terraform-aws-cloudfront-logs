f5409d7832a9012b1c3b77bcbcc731904a238015
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2023-10-21 13:14:47 +0100

    Terraform Formatting

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "target_log_group" {
-  name = var.log_group_name
+  name              = var.log_group_name
   retention_in_days = var.retention_in_days
-  kms_key_id = try(aws_kms_key.cloudwatch_logs_key[0].arn, var.cloudwatch_log_group_kms_key_arn)
+  kms_key_id        = try(aws_kms_key.cloudwatch_logs_key[0].arn, var.cloudwatch_log_group_kms_key_arn)
 }
